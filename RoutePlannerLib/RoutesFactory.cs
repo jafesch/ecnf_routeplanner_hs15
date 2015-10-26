88998c34e9d8009aba5581592446951ba455b4b5
Julian Fisch <julian.fisch@asinfotrack.ch>
2015-10-26 14:09:09 +0100

    lab 5 aufgabe 1

---
+++ RoutePlannerLib/RoutesFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RoutesFactory
+    {
+        static public IRoutes Create(Cities cities)
+        {
+            var routeAlgorithm = Properties.Settings.Default.RouteAlgorithm;
+            return Create(cities, routeAlgorithm);
+        }
+        static public IRoutes Create(Cities cities, string algorithmClassName)
+        {
+            IRoutes routeFactory = null;
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            Type t = null;
+            foreach (var assembly in assemblies)
+            {
+                t = assembly.GetType(algorithmClassName);
+                if (t != null)
+                {
+                    routeFactory = (IRoutes)Activator.CreateInstance(t, cities);
+                    return routeFactory as IRoutes;
+                }
+            }
+
+            throw new NotSupportedException(algorithmClassName + " is not supported.");
+        }
+    }
+}
